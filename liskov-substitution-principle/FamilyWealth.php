2fa010ac000542dace75ee26f5af743b4bce5d9b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 20:03:12 +0100

    Update FamilyWealth.php

--- liskov-substitution-principle/FamilyWealth.php
+++ liskov-substitution-principle/FamilyWealth.php
@@ -3,13 +3,11 @@
 We attempt to illustrate the Liskov substitution principle keeping
 in mind the following three characteristics of this principle: 
 
-(I am not sure how to implement the third one.)
-
 1. Method arguments of the subtype should be contravariant.
 2  Method return types of the subtype should be covariant.
-3. No new exceptions should be thrown by methods of the subtype, 
-except where those exceptions are themselves subtypes of exceptions.
+3. No new exceptions should be thrown by methods of the subtype, except where those exceptions are themselves subtypes of exceptions.
 
+(I am not sure how to implement the third one.)
 */
 
 declare(strict_types=1);
@@ -20,9 +18,7 @@ class FamilyWealth
 {
 	public function getWealth(GrandFatherWealthInteface $GrandFatherWealth): FamilyWealth
 	{
-	
-		// method implementation 
-		
-		return self;
+	   // method implementation 	
+	   return self;
 	}	
 }
