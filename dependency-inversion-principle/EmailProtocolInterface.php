784793617073e09e0413a7d640243797150d5a7c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 20:39:33 +0100

    Create EmailProtocolInterface.php

---
+++ dependency-inversion-principle/EmailProtocolInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace myClasses;
+
+interface EmailProtocolInterface
+{
+    
+	public function protocol(): string;
+	
+	public function connect(string $mailbox,string $user,string $password,int $flags=0,int $n_retries=0,array $options=[]): bool;
+
+}
