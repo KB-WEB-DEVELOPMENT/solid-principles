4662cf85d837b0c10eefa60f2f7925bc8ee7d7b5
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 20:44:53 +0100

    Update UserManager.php

--- dependency-inversion-principle/UserManager.php
+++ dependency-inversion-principle/UserManager.php
@@ -5,19 +5,19 @@ namespace myClasses;
 
 final class UserManager
 {
-	public function __construct(
-		private EmailProtocolInterface $emailProtocol
-    ) {}
+     public function __construct(
+        private EmailProtocolInterface $emailProtocol
+     ) {}
 	
-	private function connectToMailServer(): bool
-    {
-		if ( ($this->emailProtocol->protocol() == 'INVALID IMAP PORT NUMBER') 
-			  ||  
-		     ($this->emailProtocol->protocol() == 'INVALID POP3 PORT NUMBER')
-		   ) {
-				throw new RuntimeException('Your port number is not valid.'); 				
-		} else {			
-			return $this->emailProtocol->connect($mailbox,$user,$password,$flags,$n_retries,$options))
-		}
+     private function connectToMailServer(): bool
+     {
+        if ( ($this->emailProtocol->protocol() == 'INVALID IMAP PORT NUMBER') 
+	      ||  
+	      ($this->emailProtocol->protocol() == 'INVALID POP3 PORT NUMBER')
+	   ) {
+	        throw new RuntimeException('Your port number is not valid.'); 				
+	   } else {			
+	     return $this->emailProtocol->connect($mailbox,$user,$password,$flags,$n_retries,$options))
+	  }
     }		
 }
