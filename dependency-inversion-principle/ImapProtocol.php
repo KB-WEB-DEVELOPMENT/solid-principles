f0381bfdbcfff624489157c424ee7bed8467ab4c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 20:40:05 +0100

    Add files via upload

---
+++ dependency-inversion-principle/ImapProtocol.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace myClasses;
+
+final class ImapProtocol implements EmailProtocolInterface
+{
+	public function __construct(
+		private EmailProtocolInterface $emailProtocolInterface,
+		private int portNumber
+    ) {}
+	
+	// We could also check if the ports numbers are currently available with fsockopen()
+	public function protocol(): string 
+	{		
+		$protocol = match ($this->portNumber) {
+			143,993 => 'IMAP',
+			default => 'INVALID IMAP PORT NUMBER',
+		};
+		
+		return $protocol;
+	}
+	
+	// the port number is included in $mailbox
+	public function connect(string $mailbox,string $user,string $password,int $flags=0,int $n_retries=0,array $options=[]): bool 
+	{		
+		return is_bool(imap_open($mailbox,$user,$password,$flags,$n_retries,$options)) ? false : true;
+	}
+			
+}
---
+++ dependency-inversion-principle/UserManager.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace myClasses;
+
+final class UserManager
+{
+	public function __construct(
+		private EmailProtocolInterface $emailProtocol
+    ) {}
+	
+	private function connectToMailServer(): bool
+    {
+		if ( ($this->emailProtocol->protocol() == 'INVALID IMAP PORT NUMBER') 
+			  ||  
+		     ($this->emailProtocol->protocol() == 'INVALID POP3 PORT NUMBER')
+		   ) {
+				throw new RuntimeException('Your port number is not valid.'); 				
+		} else {			
+			return $this->emailProtocol->connect($mailbox,$user,$password,$flags,$n_retries,$options))
+		}
+    }		
+}
