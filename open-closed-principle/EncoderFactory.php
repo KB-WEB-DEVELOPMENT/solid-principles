1760bf7ec2a5ffc493ae1a17c472b16a5adcac59
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 19:53:38 +0100

    Update EncoderFactory.php

--- open-closed-principle/EncoderFactory.php
+++ open-closed-principle/EncoderFactory.php
@@ -6,23 +6,18 @@ namespace myClasses;
 
 final class EncoderFactory
 {		
-	
-	public function createEncoderForFormat(string $format): EncoderInterface
-    {
-		
-		try {
-				$encoder = match ($format) {
-					'base64' => new Base64Encoder(),
-					'url' => new  UrlEncoder(),
-				};
-		
-			} catch (\UnhandledMatchError $e) {
-				throw new InvalidArgumentException(
-					'The encoder type is unknown.'
-				);
-			}
-		
-		return $encoder;
-		
-	}
-}	
+    public function createEncoderForFormat(string $format): EncoderInterface
+    {	
+       try {
+	    $encoder = match ($format) {
+			  'base64' => new Base64Encoder(),
+			   'url' => new  UrlEncoder(),
+       		       };	
+       } catch (\UnhandledMatchError $e) {
+	   throw new InvalidArgumentException(
+	     'The encoder type is unknown.'
+	    );
+       }
+       return $encoder;		
+    }	
+}
