665e68de5a106dbdde1f1944a99df5b8c5eb59c6
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 19:39:20 +0100

    Create ZipArchiveFactory.php

---
+++ single-responsability-principle/ZipArchiveFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace myClasses;
+
+final class ZipArchiveFactory
+{
+
+	public function __construct(
+        private ZipArchive $zipArchive		
+	) {}
+
+	public function openFile(string $filename,int $flags = 0): bool
+    {
+
+		$res = $this->zipArchive->open($filename,$flags); // returns bool or int
+			
+		return ($res == true) ? true : false;
+			
+		}		
+	}
+
+	public function closeFile(): bool
+    {
+		$res = $this->zipArchive->close();
+		
+		return $res == true;
+	}	
+	
+}
