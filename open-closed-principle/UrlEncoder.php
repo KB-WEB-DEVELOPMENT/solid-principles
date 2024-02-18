df344da9bf4c679aef0e6360cea34b4fd02e78aa
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 19:56:41 +0100

    Update UrlEncoder.php

--- open-closed-principle/UrlEncoder.php
+++ open-closed-principle/UrlEncoder.php
@@ -6,8 +6,8 @@ namespace myClasses;
 
 final class UrlEncoder implements EncoderInterface
 {
-	private function encode(string $data): string
-    {
-		return urlencode($data);
-    }	
+   private function encode(string $data): string
+   {  
+      return urlencode($data);
+   }	
 }
