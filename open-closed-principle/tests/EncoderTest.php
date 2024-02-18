0917648f4b6d4a8f05066b52841ebac6d707fe9c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 19:57:46 +0100

    Create EncoderTest.php

---
+++ open-closed-principle/tests/EncoderTest.php
@@ -0,0 +1,40 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace myTests;
+
+use myClasses\EncoderInterface;
+use myClasses\Base64Encoder;
+use myClasses\UrlEncoder;
+use myClasses\GenericEncoder;
+use myClasses\EncoderFactory;
+
+use PHPUnit\Framework\TestCase;
+
+final class EncoderTest extends TestCase
+{
+    public function canEncodeString(): void
+    {
+		$factory = new EncoderFactory();
+		$encoder = (new GenericEncoder($factory))->createEncoderForFormat('url');
+		
+		$str = $encoder->encode('K â m i');
+
+		$exp = 'K+%C3%A2+m+i';
+
+		$this->assertSame($str,$exp);
+
+	}
+
+    public function cannotFindEncoder(): void
+    {
+        $factory = new EncoderFactory();
+		$encoder = (new GenericEncoder($factory))->createEncoderForFormat('wrong encoder');
+		
+		$str = $encoder->encode('K â m i'); 
+	
+		  $this->expectException(InvalidArgumentException::class);
+	}
+
+}
