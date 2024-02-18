7d49f4855c19601e8dbe201907c235a6c9fbc5ae
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 20:39:46 +0100

    Update EmailProtocolInterface.php

--- dependency-inversion-principle/EmailProtocolInterface.php
+++ dependency-inversion-principle/EmailProtocolInterface.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 namespace myClasses;
 
 interface EmailProtocolInterface
-{
-    
+{    
 	public function protocol(): string;
 	
 	public function connect(string $mailbox,string $user,string $password,int $flags=0,int $n_retries=0,array $options=[]): bool;
