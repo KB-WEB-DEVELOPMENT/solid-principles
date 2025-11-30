1a205aaa9e4d3951a383134f75ec34981ad94512
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-11-30 12:30:17 +0100

    Update MultiFormatParser.php

--- interface-segegration-principle/MultiFormatParser.php
+++ interface-segegration-principle/MultiFormatParser.php
@@ -15,106 +15,107 @@ final class MultiFormatParser implements CsvParserInterface,XmlParserInterface
     public function parseCsv(): array
     {	 						
        if ($this->isCsvReadable(
-	    $this->configCsvRead['filepath'],
-	    $this->configCsvRead['mode'],
-	    $this->configCsvRead['include_path'],
+	    	$this->configCsvRead['filepath'],
+	    	$this->configCsvRead['mode'],
+	    	$this->configCsvRead['include_path'],
             $this->configCsvRead['context']
-        ) == false) {
-	   throw new RuntimeException('Cannot open CSV file.');
-	} else {
-	    $openedCsvFile = $this->CsvFilePointResource(
-		                  $this->configCsvRead['filepath'],
-				  $this->configCsvRead['mode'],
-				  $this->configCsvRead['include_path'],
-				   $this->configCsvRead['context']
-									);
-	    try {
-		while ($this->isACsvLineReadable(
-			   $this->configCsvLineRead['stream'],
-			   $this->configCsvLineRead['length'],
-			   $this->configCsvLineRead['separator'],
-			   $this->configCsvLineRead['enclosure'],
-			   $this->configCsvLineRead['escape']
-	       ) !== false) {			
-		      $data = $this->csvLine(
-			          $this->configCsvLineRead['stream'],
-				  $this->configCsvLineRead['length'],
-				  $this->configCsvLineRead['separator'],
-				  $this->configCsvLineRead['enclosure'],
-				  $this->configCsvLineRead['escape']
-		      );								
-		      $this->parsedCsvArray[] = $data;												
-		}
+          ) == false) {
+	   			throw new RuntimeException('Cannot open CSV file.');
+	   } else {
+	    		$openedCsvFile = $this->CsvFilePointResource(
+		                  				  $this->configCsvRead['filepath'],
+										  $this->configCsvRead['mode'],
+										  $this->configCsvRead['include_path'],
+										  $this->configCsvRead['context']
+								 );	    
+				try {
+					  while ($this->isACsvLineReadable(
+					   				$this->configCsvLineRead['stream'],
+								    $this->configCsvLineRead['length'],
+								    $this->configCsvLineRead['separator'],
+								    $this->configCsvLineRead['enclosure'],
+								    $this->configCsvLineRead['escape']
+			             			) !== false)  {			
+				      						$data = $this->csvLine(
+											          	$this->configCsvLineRead['stream'],
+														$this->configCsvLineRead['length'],
+														$this->configCsvLineRead['separator'],
+														$this->configCsvLineRead['enclosure'],
+														$this->configCsvLineRead['escape']
+				      					 	);
+						
+				      					$this->parsedCsvArray[] = $data;												
+									}
 
-	     } catch (Exception $e) {
-	        echo 'The csv file can be opened but cannot be properly read. Check the file.';
-	     }
-	       
+	     			 } catch (Exception $e) {
+	        			echo 'The csv file can be opened but cannot be properly read. Check the file.';
+	     			}	       
           }		
 	
-	   if ($this->closeCsvFile($openedCsvFile) == false {
-		throw new RuntimeException('Cannot close CSV file.');
-	   } else {
-	      $this->closeCsvFile($openedCsvFile);
-           }	
-	    return $this->parsedCsvArray;
+	   	   if ($this->closeCsvFile($openedCsvFile) == false {
+				throw new RuntimeException('Cannot close CSV file.');
+	   	    } else {
+	      		$this->closeCsvFile($openedCsvFile);
+           }
+		   
+	    	return $this->parsedCsvArray;
       }
 						
       public function isCsvReadable(string $filepath,string $mode,bool $use_include_path = false,?resource $context = null):  bool
       {
-	 return is_bool(fopen($filepath,$mode,$use_include_path,$context)) ? false : true;
+		 	return is_bool(fopen($filepath,$mode,$use_include_path,$context)) ? false : true;
       }
 
       public function CsvFilePointResource(string $filepath,string $mode,bool $use_include_path = false,?resource $context = null):  resource
       {
-	return fopen($filepath,$mode,$use_include_path,$context);
+			return fopen($filepath,$mode,$use_include_path,$context);
       }	
 	
       public function isACsvLineReadable(resource $stream,?int $length = null,string $separator = ",",string $enclosure = "\"",string $escape = "\\"):  bool
       {
-	return is_bool(fgetcsv($stream,$length,$separator,$enclosure,$escape)) ? false : true;
+			return is_bool(fgetcsv($stream,$length,$separator,$enclosure,$escape)) ? false : true;
       }
 	
       public function csvLine(resource $stream,?int $length = null,string $separator = ",",string $enclosure = "\"",string $escape = "\\"):  array|bool
       {
-        return fgetcsv($stream,$length,$separator,$enclosure,$escape);
-       }	
+    		return fgetcsv($stream,$length,$separator,$enclosure,$escape);
+      }	
 	
        public function closeCsvFile(resource $stream): bool
        {
-	  return close($stream);
+	  		return close($stream);
        }	
 	
        public function parseXml(string $filename): array
        {	 
-	   libxml_use_internal_errors($this->enableErrorsHandling); // not used here
+	   		libxml_use_internal_errors($this->enableErrorsHandling); // not used here
 		
-	    try {
-		 $objXmlDocument = $this->loadXml($filename);
+	    	try {
+		 		$objXmlDocument = $this->loadXml($filename);
 		
-		  if ($objXmlDocument === FALSE) {
-		      throw new RuntimeException("Something is wrong with the XML file, it cannot be loaded. Check the file");
-		  }				
-	     }	
+		  		if ($objXmlDocument === FALSE) {
+		      		throw new RuntimeException("Something is wrong with the XML file, it cannot be loaded. Check the file");
+		  		}				
+	     	}	
 		
-	     try {
-		  $objJsonDocument = $this->jsonEncode($objXmlDocument);
+	        try {
+		  		$objJsonDocument = $this->jsonEncode($objXmlDocument);
 		
-		  if ($objJsonDocument === FALSE) {
-	             throw new RuntimeException("The XML file could not be json encoded.");
-		   }				
-	     }
+		  		if ($objJsonDocument === FALSE) {
+	             	throw new RuntimeException("The XML file could not be json encoded.");
+		   		}				
+	     	}
 		
-	     try {
-		  $arrayDocument = [];
-		  $arrayDocument = $this->jsonDecode($objJsonDocument);
+	     	try {
+		  		$arrayDocument = [];
+		  		$arrayDocument = $this->jsonDecode($objJsonDocument);
 		
-		  if ($arrayDocument === null) {
-			  throw new RuntimeException("The json file could not be decoded.");
-		  }				
-	      }
+		  		if ($arrayDocument === null) {
+			  		throw new RuntimeException("The json file could not be decoded.");
+		  		}				
+	      	}
 		
-	      return $arrayDocument;
+	      	return $arrayDocument;
 	}
 
 	public function loadXml(string $filename,?string $class_name = SimpleXMLElement::class,int $options = 0,string $namespace_or_prefix = "",bool $is_prefix = false):  SimpleXMLElement
